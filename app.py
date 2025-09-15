a27829d4ae972997c1e25ce650a4b21b605dae94
Wedding Planner <wedding@example.com>
2025-09-15 20:54:04 +0100

    Change default port from 5000 to 5070

--- app.py
+++ app.py
@@ -50,4 +50,4 @@ if __name__ == '__main__':
     with app.app_context():
         # Create tables if they don't exist
         db.create_all()
-    app.run(host='0.0.0.0', port=5000, debug=os.environ.get('FLASK_DEBUG', 'False').lower() == 'true')
+    app.run(host='0.0.0.0', port=5070, debug=os.environ.get('FLASK_DEBUG', 'False').lower() == 'true')
